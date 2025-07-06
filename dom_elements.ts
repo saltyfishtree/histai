1a7caede0182606f002b9f511e2b7a9258ceea7b
saltyfishtree <shuzhang.94@gmail.com>
2025-07-06 22:22:04 +0800

    Deleted dom_elements.ts

--- dom_elements.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @license
- * SPDX-License-Identifier: Apache-2.0
- */
-
-export const appHeader = document.getElementById('app-header') as HTMLElement;
-export const appContent = document.getElementById('app-content') as HTMLElement;
-export const appFooter = document.getElementById('app-footer') as HTMLElement;
